f509f208d371337bd2eb03c14e436b87fb83eeff
Bob Lee <bob@twingate.com>
2024-12-04 23:30:40 -0500

    fix lint by updating from depreciated go modules

--- provider/cmd/pulumi-resource-twingate/main.go
+++ provider/cmd/pulumi-resource-twingate/main.go
@@ -21,7 +21,7 @@ import (
 	_ "embed"
 
 	twingate "github.com/Twingate/pulumi-twingate/provider"
-	"github.com/pulumi/pulumi-terraform-bridge/pf/tfbridge"
+	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/pf/tfbridge"
 )
 
 //go:embed schema-embed.json
